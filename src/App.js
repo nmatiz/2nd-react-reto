2fc3a04144459c3024efd41c6926a57eb3ca3e55
norberto80@gmail.com <namatiz80@gmail.com>
2019-10-03 20:16:18 -0500

    REACT - INCREMENTA NUMERO V1.0

--- src/App.js
+++ src/App.js
@@ -2,74 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      counter: 0,
+    state = {
+      value: 0
     }
-  }
-
-  componentDidMount() {
-    const increment = document.querySelector('.js-increment');
-    if(increment) {
-      increment.addEventListener('click', this.onIncrementClick);
+    incrementar = () => { 
+      this.setState ({value: this.state.value + 1});
     }
-  }
-
-  onIncrementClick = () => {
-    this.setState({
-      counter: this.state.counter + 1,
-    });
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <div className='counter'>
-          <div className='counter__state'>
-            {this.state.counter}
-          </div>
-          <div className='coutner__buttons'>
-            <button
-              className='js-increment'
-              primary={true}
-            >
-              Incrementa
-            </button>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
-
-
-/*  
-import React, { Component } from 'react';
-import './App.css';
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      value: 0,
-
-    }
-  }
-
+  
   render() {
     return (
       <div>
         <span className="value">{this.state.value}</span>
-        <button id="inc">Incrementa</button>
+        <button id="inc" onClick={this.incrementar}>Incrementa</button>
       </div>
     );
   }
-}*/
-
-
-//this.setState({value: this.state.value + 1,});
+}
 
 export default App;
